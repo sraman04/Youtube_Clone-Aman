b1401a7c75a1c3ef7d16235607a3de5ddd3cd605
Aman <sankhwaraman650@gmail.com>
2023-07-13 11:43:18 +0530

    first commit

---
+++ yt/src/utils/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: {
+    hl: "en",
+    gl: "US",
+  },
+  headers: {
+    "X-RapidAPI-Key": "ddce78baa7mshcc0cccc4f6cd393p11c68cjsn95a138feef14",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
